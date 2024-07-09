fee65ec38921c2d3a40afc9555b63a01a87254eb
Umer qazi <uqazi844@gmail.com>
2024-07-09 13:18:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+import TicTacToe from './component/tictactoe/index'
+
+function App() {
+
+  return (
+    <>
+     <TicTacToe />
+    </>
+  )
+}
+
+export default App
